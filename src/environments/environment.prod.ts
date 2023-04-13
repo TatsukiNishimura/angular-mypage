afbf0b69b47b0e0f11e0ed416041ae3a1edbc0c8
t-nishimura <hbvcg00@gmail.com>
2023-04-13 17:58:04 +0900

    change assets path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  defaultLang: 'ja',
+  languages: [{ 'lang': 'en', 'label': 'English' }, { 'lang': 'ja', 'label': 'Japanese' }]
 };
