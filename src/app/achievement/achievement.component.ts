4c6368430a010f651790a7680c6addfd338df7d1
TatsukiNishimura <hbvcg00@gmail.com>
2024-06-12 11:49:09 +0900

    add icra2024-speackerdeck

--- src/app/achievement/achievement.component.ts
+++ src/app/achievement/achievement.component.ts
@@ -12,4 +12,17 @@ export class AchievementComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://speakerdeck.com/assets/embed.js';
+    // data-ratioとdata-idを追加
+    script.setAttribute('data-ratio', '1.7772511848341233');
+    script.setAttribute('data-id', 'e24bb6db369045d0bb22b0ac15d50413');
+    script.setAttribute('class', 'speakerdeck-embed')
+    
+    const div = document.getElementById('script');
+    div?.insertAdjacentElement('afterend', script);
+  }
+
 }
